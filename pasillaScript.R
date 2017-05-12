cf6efa64dcc8c376f2108ef9f1502880c21eb4be
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-05-12 08:20:14 -0500

    Pasilla script

---
+++ pasillaScript.R
@@ -0,0 +1,13 @@
+library(pasilla)
+data("pasillaGenes")
+countTable <- counts(pasillaGenes)
+countTable <- as.data.frame(countTable)
+
+y <- DGEList(counts=countTable)
+allGroups <- c(rep("treated",3), rep("untreated",3))
+plotMDS(y, col = c("blue","blue","blue","blue","red","red","red","red")[factor(allGroups)], cex=0.6)
+
+plotMDS(y, col = c("blue","blue","blue","blue","red","red","red","red")[factor(allGroups)], cex=0.6,
+xlim=c(-1.3, 1.3), ylim=c(-1.3, 1.3))
+
+plotMDS(y, col = c("blue","blue","blue","blue","red","red","red","red")[factor(allGroups)], cex=0.6, xlim=c(-1.3, 1.3), ylim=c(-1.3, 1.3)) + theme(aspect.ratio=1)
