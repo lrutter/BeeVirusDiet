99af1e17647f35b9c9421ff189b64cdd0b77cba1
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-05-21 15:15:27 -0500

    coord_cartesian to have ribbon go off axes

--- plotCIPI.R
+++ plotCIPI.R
@@ -7,10 +7,12 @@ mpi <- cbind(dat, predict(m, interval = "prediction"))
 plotPoints <- mpi[which(!(mpi$wt > mpi$lwr & mpi$wt < mpi$upr)),]
 
 # Create prediction interval data frame with upper and lower lines corresponding to sequence covering minimum and maximum of x values in original dataset
-newx <- seq(min(mpi$qsec), max(mpi$qsec), by=0.05)
+minX <- min(mpi$qsec)
+maxX <- max(mpi$qsec)
+newx <- seq(minX - (maxX-minX), maxX + (maxX-minX), by=0.05)
 pred_interval <- predict(m, newdata=data.frame(qsec=newx), interval="prediction", level = 0.95)
 pred_interval <- as.data.frame(pred_interval)
 
 pred_interval$qsec = newx
-ggplot(data=plotPoints, aes(x = qsec)) + geom_point(aes(y=wt)) + 
-  geom_ribbon(data=pred_interval, aes(ymin = lwr, ymax = upr), fill = "blue", alpha = 0.2) 
+ggplot(data=plotPoints, aes(x = qsec)) + geom_point(aes(y=wt)) + geom_ribbon(data=pred_interval, aes(ymin = lwr, ymax = upr), fill = "blue", alpha = 0.2) + coord_cartesian(xlim = c(minX, maxX))
+
