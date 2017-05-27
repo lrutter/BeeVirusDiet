8c430464aefdf78f77aac2243699d68207471c57
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-05-27 13:03:45 -0500

    Need inherit=FALSE when plot points on hexbins

--- tblshoot-VSNP/staticHexRLog-VSNP.R
+++ tblshoot-VSNP/staticHexRLog-VSNP.R
@@ -15,14 +15,20 @@ library(ggplot2)
 outDir = "/Users/lindz/BeeVirusDiet/tbleshoot-VSNP"
 
 my_fn <- function(data, mapping, ...){
-  x = data[,c(as.character(mapping$x))]
-  y = data[,c(as.character(mapping$y))]
+  xChar = as.character(mapping$x)
+  yChar = as.character(mapping$y)
+  x = data[,c(xChar)]
+  y = data[,c(yChar)]
   h <- hexbin(x=x, y=y, xbins=xbins, shape=1, IDs=TRUE, xbnds=maxRange, ybnds=maxRange)
   hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
   attr(hexdf, "cID") <- h@cID
   p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity") + geom_abline(intercept = 0, color = "red", size = 0.25) + coord_cartesian(xlim = c(-1*buffer, maxRange[2]+buffer), ylim = c(-1*buffer, maxRange[2]+buffer))
   p
 }
+
+p <- ggpairs(bindataSel[,-1], lower = list(continuous = my_fn))
+
+
 
 # Following DESeq2 vignette (using rlog)
 beeCounts <- read.delim(file="../AllLaneCount.txt",row.names=1,stringsAsFactors = FALSE)
@@ -36,35 +42,22 @@ dds <- DESeq(dds)
 rld <- rlog(dds)
 
 res <- results(dds, contrast=c("treatment","VS","NP"))
+degIndex <- which(res@listData$padj<0.05) 
+degData <- bindataSel[degIndex,]
 
+bindataSel <- as.data.frame(assay(rld))[, c(7:12,43:48)]
+setDT(bindataSel, keep.rownames = TRUE)[]
+colnames(bindataSel)[1] <- "ID"
+bindataSel$ID <- as.character(bindataSel$ID)
+bindataSel <- as.data.frame(bindataSel)
 
-bindata <- as.data.frame(assay(rld))
-setDT(bindata, keep.rownames = TRUE)[]
-colnames(bindata)[1] <- "ID"
-bindata$ID <- as.character(bindata$ID)
-colNames <- colnames(bindata)
-myPairs <- unique(sapply(colNames, function(x) unlist(strsplit(x,"[.]"))[1]))
-myPairs <- myPairs[-which(myPairs=="ID")]
-colGroups <- sapply(colNames, function(x) unlist(strsplit(x,"[.]"))[1])
-bindata <- as.data.frame(bindata)
-
-for (i in 1:(length(myPairs)-1)){
-  for (j in (i+1):length(myPairs)){
-    group1 = myPairs[i]
-    group2 = myPairs[j]
-    bindataSel <- cbind(ID=bindata$ID, bindata[,which(colGroups %in% c(group1, group2))])
-    maxVal = max(bindataSel[,-1])
-    minVal = min(bindataSel[,-1])
-    maxRange = c(minVal, maxVal)
-    xbins=10
-    buffer = maxRange[2]/xbins
-    p <- ggpairs(bindataSel[,-1], lower = list(continuous = my_fn))
-    jpeg(filename=paste0(outDir, "/", group1, "_", group2, ".jpg"), height=700, width=700)
-    print(p)
-    dev.off()
-  }
-}
-
-
-
+maxVal = max(bindataSel[,-1])
+minVal = min(bindataSel[,-1])
+maxRange = c(minVal, maxVal)
+xbins=10
+buffer = maxRange[2]/xbins
+p <- ggpairs(bindataSel[,-1], lower = list(continuous = my_fn))
+jpeg(filename=paste0(outDir, "/", group1, "_", group2, ".jpg"), height=700, width=700)
+print(p)
+dev.off()
 
