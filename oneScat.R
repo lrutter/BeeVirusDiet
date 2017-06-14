b723ee43dd274de126c3ebe5930b3da1686683b0
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-06-14 11:29:34 -0500

    single scat working for first gene

--- oneScat.R
+++ oneScat.R
@@ -15,12 +15,13 @@ library(DESeq2)
 
 ui <- shinyUI(fluidPage(
   #uiOutput("selInput"),
-  #actionButton("goButton", "Go!"),
+  actionButton("goButton", "Go!"),
+  verbatimTextOutput("test2"),
   #verbatimTextOutput("selectedValues"),
   plotlyOutput("scatMatPlot")
 ))
 
-server <- shinyServer(function(input, output) {
+server <- shinyServer(function(input, output, session) {
   
   dds <- readRDS("/Users/lindz/BeeVirusDiet/tblshoot-VSNP/Method1/beeDataDDSRLD.rds")[[1]]
   rld <- readRDS("/Users/lindz/BeeVirusDiet/tblshoot-VSNP/Method1/beeDataDDSRLD.rds")[[2]]
@@ -44,25 +45,8 @@ output$scatMatPlot <- renderPlotly({
   res <- results(dds, contrast=c("treatment",group1,group2))
   degIndex <- which(res@listData$padj<0.05) 
   resSort <- res[ order(res[,6]), rm.NA=TRUE]
-  g <- rownames(resSort)[1]
-  currGene <- bindataSel[which(bindataSel$ID==g),]
-  currGene1 <- unname(unlist(currGene[,sampleIndex1]))
-  currGene2 <- unname(unlist(currGene[,sampleIndex2]))
-  len <- length(currGene1)
   
-  c1 <- c()
-  c2 <- c()
-  
-  k=1
-  for (i in 1:len){
-    for (j in 1:len){
-      c1[k] <- currGene1[i]
-      c2[k] <- currGene2[j]
-      k <- k+1
-    }
-  }
-  
-  orangeDots <- data.frame(x=c1, y=c2)
+  #orangeDots <- data.frame(x=c1, y=c2)
   
   minVal = min(bindataSel[,-1])
   maxVal = max(bindataSel[,-1])
@@ -79,44 +63,79 @@ output$scatMatPlot <- renderPlotly({
   
   #p + geom_point(aes(x=x, y=y), data=orangeDots, inherit.aes = FALSE, color="orange")
   
-  c1
-  c2
-  
-    ggPS <- ggplotly(p)
+  ggPS <- ggplotly(p)
 
-    myLength <- length(ggPS[["x"]][["data"]])
-    for (i in 1:myLength){
-      hexHover = ggPS[["x"]][["data"]][[i]]$text
-      if (!is.null(hexHover) && grepl("hexID", hexHover)){
-        ggPS[["x"]][["data"]][[i]]$text <- strsplit(hexHover, "<")[[1]][1]
-        ggPS[["x"]][["data"]][[i]]$t2 <- hexHover
-        ggPS[["x"]][["data"]][[i]]$hoverinfo <- "text"
+  myLength <- length(ggPS[["x"]][["data"]])
+  for (i in 1:myLength){
+    hexHover = ggPS[["x"]][["data"]][[i]]$text
+    if (!is.null(hexHover) && grepl("hexID", hexHover)){
+      ggPS[["x"]][["data"]][[i]]$text <- strsplit(hexHover, "<")[[1]][1]
+      ggPS[["x"]][["data"]][[i]]$t2 <- hexHover
+      ggPS[["x"]][["data"]][[i]]$hoverinfo <- "text"
+    }
+  }
+  
+  datInput <- eventReactive(input$goButton, {
+    # Chose particular gene
+    g <- rownames(resSort)[1]
+    currGene <- bindataSel[which(bindataSel$ID==g),]
+    currGene1 <- unname(unlist(currGene[,sampleIndex1]))
+    currGene2 <- unname(unlist(currGene[,sampleIndex2]))
+    len <- length(currGene1)
+    
+    c1 <- c()
+    c2 <- c()
+    
+    k=1
+    for (i in 1:len){
+      for (j in 1:len){
+        c1[k] <- currGene1[i]
+        c2[k] <- currGene2[j]
+        k <- k+1
       }
     }
-    
-    ggPS %>% onRender("
-      function(el, x, data) {
+    c(c1, c2)
+  })
+  
+  output$test2 <- renderPrint({
+    str(datInput())
+  })
+  
+  observe({
+    session$sendCustomMessage(type = "lines", datInput())
+  })
+  
+  
+  ggPS %>% onRender("
+    function(el, x, data) {
 
-      // Save selected row IDs for PCP
-      //Shiny.onInputChange('selID', selID);
-      console.log('here!')
-console.log(data.c1)
-console.log(data.c2)
-      var Traces = [];
-      var trace = {
-      x: data.c1,
-      y: data.c2,
-      mode: 'markers',
-      marker: {
-      color: 'orange',
-      size: 6
-      },
-      hoverinfo: 'none'
-      };
-      Traces.push(trace);
-      Plotly.addTraces(el.id, Traces);
-      }
-      ", data = list(c1=c1, c2=c2))
+    // Save selected row IDs for PCP
+    //Shiny.onInputChange('selID', selID);
+
+Shiny.addCustomMessageHandler('lines',
+    function(drawLines) {
+    
+    //vecLength <- drawLines.length
+    //console.log(vecLength)
+    console.log(drawLines.length)
+    console.log(drawLines.length/2)
+    console.log(drawLines.slice(0, drawLines.length/2))
+
+    var Traces = [];
+    var trace = {
+    x: drawLines.slice(0, drawLines.length/2),
+    y: drawLines.slice(drawLines.length/2, drawLines.length),
+    mode: 'markers',
+    marker: {
+    color: 'orange',
+    size: 3
+    },
+    hoverinfo: 'none'
+    };
+    Traces.push(trace);
+    Plotly.addTraces(el.id, Traces);
+    });
+    }" )
   })
 })
 
