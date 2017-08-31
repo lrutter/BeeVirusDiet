7de0b8b9dccfcd661556a6c3ef432a1bd3aca02b
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-08-30 20:47:08 -0500

    data reordering

--- tblshoot/DashBoard/DashboardBees/appLog/server.R
+++ tblshoot/DashBoard/DashboardBees/appLog/server.R
@@ -12,13 +12,6 @@ dat <- readRDS("/Users/lindz/bigPint/tblshoot/AllPairs/data_limma.Rds")
 dat <- dat$counts
 dat <- as.data.frame(dat)
 
-# log() method makes boxplot look terrible
-#setDT(dat, keep.rownames = TRUE)[]
-#colnames(dat)[1] <- "ID"
-#dat <- as.data.frame(dat)
-#datCol <- colnames(dat)[-which(colnames(dat) %in% "ID")]
-#dat[,2:ncol(dat)] <- log(dat[,2:ncol(dat)]+1)
-
 # rlog() method requires dds object
 coldata = data.frame(row.names = colnames(dat), virus = unlist(lapply(colnames(dat), function (x) substring(unlist(strsplit(x, "[.]"))[1],1,1))), diet = unlist(lapply(colnames(dat), function (x) substring(unlist(strsplit(x, "[.]"))[1],2,2))), treatment = unlist(lapply(colnames(dat), function (x) unlist(strsplit(x, "[.]"))[1])))
 dds = DESeqDataSetFromMatrix(countData = dat, colData = coldata, design = ~ treatment)
